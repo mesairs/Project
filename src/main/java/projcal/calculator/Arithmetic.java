804962bd36c962bf1a537ba58c593a5a7b63a6dd
Aspire <Aspire@192.168.137.88>
2023-11-13 15:33:57 +0800

    "

---
+++ src/main/java/projcal/calculator/Arithmetic.java
@@ -0,0 +1,14 @@
+package projcal.calculator;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aspire
+ */
+public class Arithmetic {
+    
+}
