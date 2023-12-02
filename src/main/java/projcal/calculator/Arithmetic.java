3c30470379d3965ef17d081e5b5ef8a156cef92c
Aspire <Aspire@192.168.137.88>
2023-12-02 10:06:32 +0800

    Roxanne and Robie

--- src/main/java/projcal/calculator/Arithmetic.java
+++ src/main/java/projcal/calculator/Arithmetic.java
@@ -12,4 +12,23 @@ package projcal.calculator;
 public class Arithmetic {
     // Add arithmetic methods
     // methods such as add, subtract, multiply, division
+    public void Addition(int x, int y){
+        int Answer = x + y;
+         System.out.println("Result: " + Answer);
+    }
+    
+    public void Subtraction(int x, int y){
+        int Answer = x - y;
+        System.out.println("Result: " + Answer);
+    }
+    
+    public void Multiplication(int x, int y){
+        int Answer = x * y;
+        System.out.println("Result: " + Answer);
+    }
+    
+    public void Division(int x, int y){
+        int Answer = x/y;
+        System.out.println("Result: " + Answer);
+    }
 }
